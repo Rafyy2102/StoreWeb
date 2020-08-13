292962614728be93013057c00dc0f95fa0a4d32e
Rafyy2102 <rafpromais@yahoo.com.br>
2020-08-13 15:06:58 -0300

    att

---
+++ StoreWeb/Store.Web/Store.Web/Upload.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Upload.aspx.cs" Inherits="Store.Web.Upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Selecione um Arquivo "></asp:Label>
+            <asp:FileUpload ID="fuArquivo" runat="server" />
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Nome do Arquivo "></asp:Label>
+            <asp:TextBox ID="txbNome" runat="server" Width="231px"></asp:TextBox>
+        </div>
+        <p>
+            <asp:Button ID="btnEnviar" runat="server" OnClick="BtnEnviar" Text="Enviar Arquivo" Width="349px" />
+            </p>
+    </form>
+</body>
+</html>
---
+++ StoreWeb/Store.Web/Store.Web/Upload.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Store.Web
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnEnviar(object sender, EventArgs e)
+        {
+            try
+            {
+                //string do upload do arquivo
+                String nome = fuArquivo.FileName;
+                //caminho do arquivo
+                String caminho = Server.MapPath(@"upload\");
+                //nome do arquivo    
+                txbNome.Text = nome;
+                //salvar na pasta upload o arquivo e o nome
+                fuArquivo.PostedFile.SaveAs(caminho + nome);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
